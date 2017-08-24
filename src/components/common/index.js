78983942441c9ea1bf2f3b17ecf897acd56815ce
Wang Bhotia <wangbhotia@gmail.com>
2017-08-23 22:35:38 -0600

    Employe Manager - React Native App

---
+++ src/components/common/index.js
@@ -0,0 +1,9 @@
+// imports and exports immediately
+
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
+export * from './Confirm';
